74c8d68e4d491fdf26326365ed7a361781367922
Quentin Widlocher <qwidlocher@gmail.com>
2020-02-06 14:07:05 +0100

    :sparkles: Synchronization

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const board = [];
 
 let currentPlayerHTML;
 
+let gameId = '';
+let playerColor = 'red';
+let unsubscribe;
+
 // TODO: Add endgame detection
 
 /**
@@ -12,7 +16,7 @@ let currentPlayerHTML;
 let putInColumn = (column) => {
     let firstAvailableRow = -1;
 
-    for (let i = (BOARD_SIZE - 1); i >= 0; i--) {
+    for (let i = BOARD_SIZE - 1; i >= 0; i--) {
         if (!board[i][column].className) {
             firstAvailableRow = i;
             break;
@@ -24,10 +28,78 @@ let putInColumn = (column) => {
             currentPlayerHTML.innerText = 'Yellow';
         } else {
             board[firstAvailableRow][column].className = 'yellow';
-            currentPlayerHTML.innerText = 'red'
+            currentPlayerHTML.innerText = 'red';
         }
+
+        gamesStore.doc(gameId).set({
+            player: playerColor,
+        });
     }
 };
+
+function subscribe(id) {
+    unsubscribe = gamesStore.doc(id).onSnapshot((snap) => {
+        console.debug('in snap', snap.data().player);
+        // Update only if turn is played byt the other player
+        if (snap.data().player !== playerColor) {
+            console.debug('other player played');
+        }
+    });
+}
+
+function loading(state) {
+    document.querySelectorAll('.spinner-border').forEach((loader) => {
+        loader.style.display = state ? 'inline-block' : 'none';
+    });
+}
+
+function newGame() {
+    loading(true);
+
+    if (unsubscribe) unsubscribe();
+
+    gamesStore
+        .add({
+            player: 'red',
+            grid: new Array(BOARD_SIZE).fill('0'.repeat(BOARD_SIZE)),
+            winner: '',
+        })
+        .then((snap) => {
+            gameId = snap.id;
+            document.querySelector('#game-id').value = snap.id;
+            playerColor = 'red';
+            document.querySelector('#playerColor').innerText = playerColor;
+            subscribe(snap.id);
+            loading(false);
+        });
+}
+
+function connect() {
+    loading(true);
+
+    if (unsubscribe) unsubscribe();
+
+    const writtenId = document.querySelector('#game-id').value;
+
+    console.debug('attempting to connect to', writtenId);
+
+    gamesStore
+        .doc(writtenId)
+        .get()
+        .then((snap) => {
+            gameId = snap.id;
+            playerColor = 'yellow';
+            document.querySelector('#playerColor').innerText = playerColor;
+            subscribe(snap.id);
+            loading(false);
+        })
+        .catch((e) => {
+            gameId = '';
+            console.error('unable to connect to game ' + writtenId, e);
+            document.querySelector('#game-id').value = '';
+            loading(false);
+        });
+}
 
 window.onload = () => {
     const tbody = document.getElementById('board');
@@ -65,5 +137,5 @@ window.onload = () => {
     }
 
     currentPlayerHTML = document.getElementById('currentPlayer');
-    currentPlayerHTML.innerText = 'Yellow';
+    currentPlayerHTML.innerText = playerColor;
 };
--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBhFeckCZ5w4OzMVI4Vr1AA2Tm6_26V0UA",
-    authDomain: "connect4-bd651.firebaseapp.com",
-    databaseURL: "https://connect4-bd651.firebaseio.com",
-    projectId: "connect4-bd651",
-    storageBucket: "connect4-bd651.appspot.com",
-    messagingSenderId: "881904021212",
-    appId: "1:881904021212:web:26a6c4721fe7e3f4118e19"
+    apiKey: 'AIzaSyBhFeckCZ5w4OzMVI4Vr1AA2Tm6_26V0UA',
+    authDomain: 'connect4-bd651.firebaseapp.com',
+    databaseURL: 'https://connect4-bd651.firebaseio.com',
+    projectId: 'connect4-bd651',
+    storageBucket: 'connect4-bd651.appspot.com',
+    messagingSenderId: '881904021212',
+    appId: '1:881904021212:web:26a6c4721fe7e3f4118e19',
 };
 firebase.initializeApp(firebaseConfig);
+const gamesStore = firebase.firestore().collection('games');
